1f43e7061a0f6011b35b5334b43a7161be9bc7bc
LuisDani <spartandanii117@gmail.com>
2023-11-27 20:57:46 -0600

    pedro es puto

--- app/src/main/java/com/dim/iundex/MainActivity.kt
+++ app/src/main/java/com/dim/iundex/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("Pedro me pela la verga puto negro de mierda:PPPPP")
     }
 }
